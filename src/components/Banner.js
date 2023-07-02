70bd3876fa713f82ef84e20678cf527bdc0f3e39
dev-web-jakekim <dev-web-jakekim@gmail.com>
2023-07-02 23:12:18 +0900

    added category component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -17,8 +17,8 @@ const Banner = () => {
     const fetchData = async () => {
         const res = await axios.get(requests.fetchNowPalying);
         const movieId = res.data.results[
-          Math.floor(Math.random() * res.data.results.length)
-          ].id
+            Math.floor(Math.random() * res.data.results.length)
+            ].id
         const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
             params: { append_to_response: "videos" }
         })
@@ -31,53 +31,53 @@ const Banner = () => {
 
     if (isClicked) {
         return (
-          <>
-              <Container>
-                  <HomeContainer>
-                      <Iframe
-                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
-                        width="640"
-                        height="360"
-                        frameborder="0"
-                        allow="autoplay; fullscreen"
-                      ></Iframe>
-                  </HomeContainer>
-              </Container>
-              <button onClick={() => setIsClicked(false)}>X</button>
-          </>
+            <>
+                <Container>
+                    <HomeContainer>
+                        <Iframe
+                            src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=0&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                            width="640"
+                            height="360"
+                            frameborder="0"
+                            allow="autoplay; fullscreen"
+                        ></Iframe>
+                    </HomeContainer>
+                </Container>
+                <button onClick={() => setIsClicked(false)}>X</button>
+            </>
         )
     } else {
         return (
-          <header
-            className='banner'
-            style={{
-                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
-                backgroundPosition: "top center",
-                backgroundSize: "cover"
-            }}
-          >
-              <div style={{ fontSize: 10 }} className='banner__contents'>
-                  <h1 className='banner__title'>
-                      {movie.title || movie.name || movie.original_name}
-                  </h1>
+            <header
+                className='banner'
+                style={{
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+                    backgroundPosition: "top center",
+                    backgroundSize: "cover"
+                }}
+            >
+                <div style={{ fontSize: 10 }} className='banner__contents'>
+                    <h1 className='banner__title'>
+                        {movie.title || movie.name || movie.original_name}
+                    </h1>
 
-                  <div className='banner__buttons'>
-                      {movie?.videos?.results[0]?.key &&
-                      <button
-                        className='banner__button play'
-                        onClick={() => setIsClicked(true)}
-                      >
-                          Play
-                      </button>
-                      }
+                    <div className='banner__buttons'>
+                        {movie?.videos?.results[0]?.key &&
+                        <button
+                            className='banner__button play'
+                            onClick={() => setIsClicked(true)}
+                        >
+                            Play
+                        </button>
+                        }
 
-                  </div>
-                  <p className='banner__description'>
-                      {truncate(movie.overview, 100)}
-                  </p>
-              </div>
-              <div className='banner--fadeBottom' />
-          </header>
+                    </div>
+                    <p className='banner__description'>
+                        {truncate(movie.overview, 100)}
+                    </p>
+                </div>
+                <div className='banner--fadeBottom' />
+            </header>
         )
     }
 
@@ -87,32 +87,32 @@ export default Banner;
 
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-    height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
 `;
 
 const HomeContainer = styled.div`
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
 `;
 
 const Iframe = styled.iframe`
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.65;
+  border: none;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
     height: 100%;
-    z-index: -1;
-    opacity: 0.65;
-    border: none;
-
-    &::after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-    }
+  }
 `;
